6e3815faa83a5cf24e462c2aa2d8cea8263edc7c
Tianyu Liu <poohlty@sina.com>
2016-04-21 23:00:39 -0400

    Add simple implementation for workers

--- sharded.py
+++ sharded.py
@@ -42,12 +42,18 @@
 
 
 import json
-
+from flask import Flask
+import thread
 
 STATE_READY = 1
 STATE_RUNNING = 2
 STATE_COMPLETE = 3
 
+STATE_STRINGS = {
+    STATE_READY: "State ready",
+    STATE_RUNNING: "State running",
+    STATE_COMPLETE: "State complete"
+}
 
 class Nexus(object):
     """
@@ -121,15 +127,81 @@ class Worker(object):
     """The remote server, this accepts work from a nexus and runs it."""
 
     def __init__(self):
+        self.app = Flask(__name__)
+        self.app.add_url_rule('/<action>', view_func = self.heartbeat_handler)
+
+        self.state = 0
+        self.result = None
 
         #
         # TODO : Should start a threadpool to execute tasks in, then call
         #        self.start()
         #
 
-        pass
+    """
+    Handles computation requests and periodic pings from master.
+    When a computation job is finished, heartbeat will also return the result.
+    When a new computation comes in, heartbeat_handler will dispatch the job and
+    mark worker as running.
+    Otherwise, heartbeat just reports worker's status to master.
+    """
+    def heartbeat_handler(self, action):
+        if action == "heartbeat":
+            # If worker has completed a job, return self.result with this
+            # heartbeat. Otherwise, report self.state.
+
+            #
+            # TODO : Need to think more on how state changes after a job is done.
+            # Might be helpful to add STATE_RETURNED.
+            #
+            if self.state == STATE_COMPLETE:
+                return self.result
+            else:
+                return STATE_STRINGS[self.state]
+
+        elif action == "computation":
+            # Initial computation request. Only accept when worker is free.
+            if (self.state == STATE_RUNNING):
+                return "Worker busy"
+            else:
+                #
+                # TODO : Extract runnable_string from payload
+                #
+                runnable_string = ""
+
+                # Dispatch a new thread with do_computation
+                thread.start_new_thread(self.do_computation, (runnable_string, ))
+                return "Job starts running"
+        else:
+            return 'Unsupported action'
+
+    """
+    Wrapper function to run actual computations
+    """
+    def do_computation(self, runnable_string):
+        #
+        # TODO : Unserialize runnable_string, create a Runnable instance and
+        # execute in current thread. Also need to sync updates to self.state
+        # and self.result
+        #
+
+        self.state = STATE_RUNNING
+
+        # Dummy job that takes 10 seconds
+        time.sleep(10)
+
+        # Syncing state is probably sufficient
+        self.result = "Dummy result"
+        self.state = STATE_COMPLETE
 
     def start(self):
+        self.state = STATE_READY
+
+        #
+        # NOTE: May or may not be necessary to set threaded = True, since the request
+        # handler could just be a single thread
+        #
+        self.app.run(threaded = True)
 
         #
         # TODO : should start a flask server a print the location / password.
@@ -140,8 +212,6 @@ class Worker(object):
         # NOTE : this generates a password (random string), reject any requests
         #        that lack the password.
         #
-
-        pass
 
     # http://lybniz2.sourceforge.net/safeeval.html -- consider using this to pass
     # globals / locals found in f.func_globals
